8ff3a7f6ace48676ae4bb3949d89e60e4eba525b
ApplesForRyuk <murtov21@gmail.com>
2022-11-21 13:05:38 +0200

    Bullseye app

---
+++ app/src/main/java/com/yourcompany/bullseye/AboutActivity.kt
@@ -0,0 +1,24 @@
+package com.yourcompany.bullseye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yourcompany.bullseye.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = getString(R.string.about_page_title)
+
+        binding.goBackButton?.setOnClickListener {
+            finish()
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/yourcompany/bullseye/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.yourcompany.bullseye
+
+ import android.content.Intent
+ import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import androidx.appcompat.app.AlertDialog
+ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+ import com.yourcompany.bullseye.databinding.ActivityMainBinding
+import java.lang.Math.abs
+ import java.lang.Math.random
+ import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private var sliderValue = 0
+    private var targetValue = newTargetValue()
+    private var totalScore = 0
+    private var currentRound = 1
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        startNewGame()
+
+
+        binding.hitMeButton.setOnClickListener {
+            showResults()
+            totalScore =+ pointsForCurrentRound()
+            binding.gameScoreTextView?.text =totalScore.toString()
+
+        }
+
+
+        binding.startOverButton?.setOnClickListener {
+            startNewGame() }
+
+        binding.infoButton?.setOnClickListener {
+            navigateToAboutPage()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekbar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sliderValue = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+
+
+    }
+
+    private fun navigateToAboutPage() {
+        val intent = Intent(this,AboutActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun differenceAmount() = kotlin.math.abs(targetValue - sliderValue)
+
+    private fun newTargetValue() = Random.nextInt(1,100)
+
+
+
+    private fun pointsForCurrentRound(): Int {
+
+        val maxScore = 100
+
+        val difference= differenceAmount()
+
+        var bonus = 0
+        if(difference==0) {
+            bonus =100
+        } else if(difference==1) {
+            bonus =50
+        }
+
+        return maxScore-difference + bonus
+
+    }
+
+    private fun startNewGame() {
+        totalScore = 0
+        currentRound = 1
+        sliderValue = 50
+        targetValue = newTargetValue()
+
+        binding.targetTextView.text = targetValue.toString()
+        binding.gameRoundTextView?.text = currentRound.toString()
+        binding.gameScoreTextView?.text = totalScore.toString()
+        binding.seekBar.progress = sliderValue
+
+
+    }
+
+    private fun showResults() {
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage =
+            getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
+
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, listener ->
+            dialog.dismiss()
+
+            targetValue= newTargetValue()
+            binding.targetTextView.text = targetValue.toString()
+
+            currentRound+=1
+            binding.gameRoundTextView?.text = currentRound.toString()
+
+        }
+
+        builder.create().show()
+    }
+
+    private fun alertTitle(): String {
+        val difference = differenceAmount()
+        val title: String = if (difference==0) {
+            getString(R.string.alert_title_1)
+        }
+        else if (difference<5) {
+            getString(R.string.alert_title_2)
+        }
+        else if (difference<=10) {
+            getString(R.string.alert_title_3)
+        }
+        else {
+            getString(R.string.alert_title_4)
+        }
+
+        return title
+
+    }
+}
